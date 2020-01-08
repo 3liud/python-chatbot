ffa038b2549d683efd3273ca4828b394c3f004a5
3liud <ssocratos@gmail.com>
2020-01-08 12:55:53 +0300

    update on the training module

--- training_chatbot.py
+++ training_chatbot.py
@@ -25,5 +25,24 @@ for intent in intents['intents']:
         if intent['tag'] not in classes: # add to our classes list
             classes.append(intent['tag'])
 
+# lemmatize, lower each word and remove duplicates
+words = [
+    lemmatizer.lemmatize(w.lower()) 
+    for w in words if w not in ignore_words
+    ]
+words = sorted(list(set(words)))
+
+#sort classes
+classes = sorted(list(set(classes)))
+# documents = combination between patterns and intents
+print(len(documents), "documents")
+# classes = intents
+print (len(classes), "classes", classes)
+# words = all words, vocabulary
+print(len(words), "unique lemmatized words", words)
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
 
 
