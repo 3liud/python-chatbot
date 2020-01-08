915a0a6563b4ae7e5a2f1359daa6e538df33e05a
3liud <ssocratos@gmail.com>
2020-01-08 14:04:01 +0300

    completed bot

--- training_chatbot.py
+++ training_chatbot.py
@@ -12,6 +12,7 @@ import random
 
 words = []
 classes = []
+documents = []
 ignore_words = ['?', '!']
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
@@ -20,6 +21,7 @@ intents = json.loads(data_file)
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         w = nltk.word_tokenize(pattern) # tokenize each word
+        words.extend(w)
         documents.append((w, intent['tag'])) # add documents in the corpus
 
         if intent['tag'] not in classes: # add to our classes list
@@ -43,6 +45,60 @@ print(len(words), "unique lemmatized words", words)
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
+
+
+# creating the training data
+training = []
+# creating an empty array for the output
+output_empty = [0] * len(classes)
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialise our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # lemmatize each word - create base word, in attempt ot represet related words
+    pattern_words = [
+        lemmatizer.lemmatize(word.lower()) 
+    for word in pattern_words
+    ]
+    # creates our bag of words array with 1, if word match found in current pattern 
+for w in words:
+    bag.append(1) if w in pattern_words else bag.append(0)
+    # the output is a 0 for each tag and 1 for current tag (each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+#create train and test lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model 
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("model created")
+
+
 
 
 
